14ac5334d1603605cccbd9e661eb8eb43989bb9e
JEaLiFe <jealife.pictures@gmail.com>
2023-08-12 21:26:46 +0200

    Changes to be committed: 	modified:   assets/css/general.css 	modified:   assets/css/style.css 	new file:   assets/images/Well, let's check what's wrong with this car@3x.jpg 	new file:   assets/js/nav.js 	modified:   index.html

---
+++ assets/js/nav.js
@@ -0,0 +1,7 @@
+//menu mobile Script
+const menu = document.querySelector('.menu');
+function showMenu() {
+    var btnMenu = document.getElementById("hamburger_menu");
+    menu.classList.toggle("menu_mobile");
+
+}
