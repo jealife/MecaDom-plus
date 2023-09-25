6956b05ecb755e079a8ef62bf1fbd1301a4296f2
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2023-09-25 16:56:49 +0000

    Changes to be committed: 	modified:   assets/css/services.css 	modified:   assets/js/filter.js 	modified:   pages/services/services.html

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,26 +1,54 @@
 function myapp() {
-    const buttons = document.querySelectorAll(".button");
-    const cards = document.querySelectorAll(".card");
-  
-    function filter(category, items) {
-      items.forEach((item) => {
-        const isItemFiltered = !item.classList.contains(category);
-        const isShowAll = category.toLowerCase() === "all";
-        if (isItemFiltered && !isShowAll) {
-          item.classList.add("hidden");
-        } else {
-          item.classList.remove("hidden");
-        }
-      });
-    }
-  
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const currentCategory = button.dataset.filter;
-        console.log(currentCategory);
-        filter(currentCategory, cards);
-      });
+  const buttons = document.querySelectorAll(".button");
+  const cards = document.querySelectorAll(".card");
+
+  function filter(category, items) {
+    items.forEach((item) => {
+      const isItemFiltered = !item.classList.contains(category);
+      const isShowAll = category.toLowerCase() === "all";
+      if (isItemFiltered && !isShowAll) {
+        item.classList.add("hidden");
+      } else {
+        item.classList.remove("hidden");
+      }
     });
   }
-  
-  myapp();
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const currentCategory = button.dataset.filter;
+      console.log(currentCategory);
+      filter(currentCategory, cards);
+    });
+  });
+}
+
+myapp();
+
+const slider = document.querySelector('.filter');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
